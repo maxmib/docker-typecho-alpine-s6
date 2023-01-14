6df11b3cad24480f7214aa01fe69073de9759df7
maxmib <30944505+maxmib@users.noreply.github.com>
2023-01-14 19:19:46 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,18 +6,18 @@ echo "begin build full version ..."
 docker image rm -f 80x86/typecho:amd64
 docker image rm -f registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:amd64
 docker image prune -f
-docker build -f ./docker/Dockerfile -t 80x86/typecho:amd64 .
-docker push 80x86/typecho:amd64
-docker tag 80x86/typecho:amd64 registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:amd64
-docker push registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:amd64
+docker build -f ./docker/Dockerfile -t maxmib/typecho:amd64 .
+docker push maxmib/typecho:amd64
+#docker tag maxmib/typecho:amd64 registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:amd64
+#docker push registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:amd64
 
-docker image rm -f 80x86/typecho:arm64
+docker image rm -f maxmib/typecho:arm64
 docker image rm -f registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:arm64
 docker image prune -f
-docker build -f ./docker/Dockerfile.arm64 -t 80x86/typecho:arm64 .
-docker push 80x86/typecho:arm64
-docker tag 80x86/typecho:arm64 registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:arm64
-docker push registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:arm64
+docker build -f ./docker/Dockerfile.arm64 -t maxmib/typecho:arm64 .
+docker push maxmib/typecho:arm64
+#docker tag maxmib/typecho:arm64 registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:arm64
+#docker push registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:arm64
 echo "end build full version ..."
 
 ./multi.sh
