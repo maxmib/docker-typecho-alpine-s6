c951a8e1f92de20911635f67a6e364b710ac0068
maxmib <30944505+maxmib@users.noreply.github.com>
2023-01-15 14:52:35 +0800

    Create build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ docker run --rm --privileged multiarch/qemu-user-static:register --reset
 
 echo "begin build full version ..."
 docker image rm -f maxmib/typecho:amd64
-docker image rm -f registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:amd64
+#docker image rm -f registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:amd64
 docker image prune -f
 docker build -f ./docker/Dockerfile -t maxmib/typecho:amd64 .
 docker push maxmib/typecho:amd64
@@ -12,7 +12,7 @@ docker push maxmib/typecho:amd64
 #docker push registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:amd64
 
 docker image rm -f maxmib/typecho:arm64
-docker image rm -f registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:arm64
+#docker image rm -f registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:arm64
 docker image prune -f
 docker build -f ./docker/Dockerfile.arm64 -t maxmib/typecho:arm64 .
 docker push maxmib/typecho:arm64
